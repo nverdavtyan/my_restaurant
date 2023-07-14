f6b2d4c23db7a6a854afd154bbbfde083b81b407
nverdavtyan <nverdavtyan1994@gmail.com>
2023-07-14 02:41:21 +0200

    fix env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  googleApiKey: 'AIzaSyD4iSJDtbG6yQ7rPEAtLDcK0BTnbTYhDJM',
   apiUrl: 'https://epitech-my-restaurant-3d212d161a78.herokuapp.com'
 };
