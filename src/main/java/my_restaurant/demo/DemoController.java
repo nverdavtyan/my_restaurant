7cb0c1a98423ed68e9335aa6108fdd65db6cff9a
nverdavtyan <nverdavtyan1994@gmail.com>
2023-08-02 12:11:29 +0200

    add docker

--- src/main/java/my_restaurant/demo/DemoController.java
+++ src/main/java/my_restaurant/demo/DemoController.java
@@ -1,21 +1,43 @@
 package my_restaurant.demo;
 
 import io.swagger.v3.oas.annotations.Hidden;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/demo-controller")
+@RequestMapping
 @Hidden
-@PreAuthorize("hasRole('ADMIN')")
 public class DemoController {
 
-  @GetMapping
-  public ResponseEntity<String> sayHello() {
-    return ResponseEntity.ok("Hello from secured endpoint");
+  private final String easterEgg = """
+                    ,---------------------------,
+                    |  /---------------------\\  |
+                    | |                       | |
+                    | |       Welcome to      | |
+                    | |     My Restaurant     | |
+                    | |         API           | |
+                    | |                       | |
+                    |  \\_____________________/  |
+                    |___________________________|
+                  ,---\\_____     []     _______/------,
+                /         /______________\\           /|
+              /___________________________________ /  | ___
+              |                                   |   |    )
+              |  _ _ _                 [-------]  |   |   (
+              |  o o o                 [-------]  |  /    _)_
+              |__________________________________ |/     /  /
+          /-------------------------------------/|      ( )/
+        /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /
+      /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""";
+
+  @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+  public ResponseEntity<String> showEasterEgg() {
+    return ResponseEntity.ok(easterEgg);
   }
 
 }
